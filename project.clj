d616e6d7ffbd1aaa9972be46531148c07be22d52
Sergey Bolgov <sbolgov@yahoo.com>
2022-05-22 22:54:40 +0300

    Empty project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject patients "0.0.1-SNAPSHOT"
+  :description "Patients CRUD"
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/licenses/gpl.html"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 ]
+  :profiles {:dev {:plugins []
+                   :dependencies []
+                   :source-paths ["dev"]}})
