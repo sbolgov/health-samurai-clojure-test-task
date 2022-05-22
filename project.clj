04a8fcfc2a010af6c1cdc57279df7c1946b71aca
Sergey Bolgov <sbolgov@yahoo.com>
2022-05-22 22:54:40 +0300

    Trivial web server works.

--- project.clj
+++ project.clj
@@ -2,10 +2,12 @@
   :description "Patients CRUD"
   :license {:name "GNU General Public License"
             :url "http://www.gnu.org/licenses/gpl.html"}
+  :main patients.main
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [com.github.seancorfield/next.jdbc "1.2.780"]
                  [org.postgresql/postgresql "42.3.6"]
                  [environ "1.2.0"]
+                 [http-kit "2.5.3"]
                  ]
   :profiles {:dev {:plugins [[lein-environ "0.4.0"]
                             ]
