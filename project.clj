a8b24236c4b23615d61ccbbe4983f2cd81917a40
Sergey Bolgov <sbolgov@yahoo.com>
2022-05-29 23:00:16 +0300

    Added postgresql.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :license {:name "GNU General Public License"
             :url "http://www.gnu.org/licenses/gpl.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.postgresql/postgresql "42.3.6"]
                  ]
   :profiles {:dev {:plugins []
                    :dependencies []
