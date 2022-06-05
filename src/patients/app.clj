dd28c89cc7e6e5b98b3b9733029bfdebe60c3824
Sergey Bolgov <sbolgov@yahoo.com>
2022-06-05 04:37:46 +0300

    Added ring-params/wrap-params.

--- src/patients/app.clj
+++ src/patients/app.clj
@@ -1,8 +1,11 @@
 (ns patients.app
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.middleware.params :as ring-params]
 ))
 
-(defroutes app
+(defroutes app-routes
   (GET "/" [] "Hello Compojure World")
   (route/not-found "Not Found"))
+
+(def app (ring-params/wrap-params app-routes))
